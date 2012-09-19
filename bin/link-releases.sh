16e2fc02b9d7d27dc1cdc9a5e56ed8856732e0eb
chris grzegorczyk <grze@eucalyptus.com>
2012-09-19 01:32:07 -0700

    fix links

--- bin/link-releases.sh
+++ bin/link-releases.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-for f in features/*/3.2; do ln -sfv $f releases/3.2/$(dirname ${f/features/}); done
-for f in features/*/3.3; do ln -sfv $f releases/3.3/$(dirname ${f/features/}); done
+(cd $(git rev-parse --show-toplevel);
+for f in features/*/3.2; do ln -sfv ../../$f releases/3.2/$(dirname ${f/features/}); done
+for f in features/*/3.3; do ln -sfv ../../$f releases/3.3/$(dirname ${f/features/}); done
+)
