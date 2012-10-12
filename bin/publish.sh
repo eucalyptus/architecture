967b43e09032d8d841c45ba9c259bf94c4a36636
chris grzegorczyk <grze@eucalyptus.com>
2012-10-11 23:49:32 -0700

    add submodule

--- bin/publish.sh
+++ bin/publish.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 BASEDIR=$(git rev-parse --show-toplevel)
-TMPDIR=$(mktemp -d /tmp/wiki-XXXXX)
-git clone --depth=1 git@github.com:EucalyptusSystems/architecture.wiki.git ${TMPDIR}
-rm -rfv ${TMPDIR}/*
+TMPDIR=${BASEDIR}/wiki
 for f in $(cd ${BASEDIR}; find .  -type f  | egrep -v '*\.(pdf|zip|wsdl|git|/\.|/bin/|/releases/|/lib/)' | sed 's/^\.\///g'); do 
   t="${f//\//:}"
-  cp -fv ${BASEDIR}/$f ${TMPDIR}/$t
+  if diff ${BASEDIR}/$f ${TMPDIR}/$t 2>&1 >/dev/null; then
+    cp -fv ${BASEDIR}/$f ${TMPDIR}/$t
+  fi
 done
 (cd ${TMPDIR}; git add *; git commit -m 'updated'; git push)
-rm -rfv ${TMPDIR}
+git add ${TMPDIR}; git commit -m 'update wiki'; git push
