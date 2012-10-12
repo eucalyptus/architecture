e60351124e529437b06e46303442ae1f9b105af4
chris grzegorczyk <grze@eucalyptus.com>
2012-10-12 00:07:01 -0700

    update wiki

--- bin/publish.sh
+++ bin/publish.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 BASEDIR=$(git rev-parse --show-toplevel)
 TMPDIR=${BASEDIR}/wiki
-for f in $(cd ${BASEDIR}; find .  -type f  | egrep -v '*\.(pdf|zip|wsdl|git|/\.|/bin/|/releases/|/lib/)' | sed 's/^\.\///g'); do 
+echo > ${BASEDIR}/.wiki.log
+for f in $(cd ${BASEDIR}; find .  -type f  | egrep -v '*\.(pdf|zip|wsdl|git|/\.|/bin/|/releases/|/lib/|.keep|TODO|README.wiki)' | sed 's/^\.\///g'); do 
   t="${f//\//:}"
-  if diff ${BASEDIR}/$f ${TMPDIR}/$t 2>&1 >/dev/null; then
-    cp -fv ${BASEDIR}/$f ${TMPDIR}/$t
+  if diff ${BASEDIR}/$f $t 2>&1 >/dev/null; then
+    cp -fv ${BASEDIR}/$f ${TMPDIR}/$t >> ${BASEDIR}/.wiki.log >> ${BASEDIR}/.wiki.log 2>&1
   fi
 done
-(cd ${TMPDIR}; git add *; git commit -m 'updated'; git push)
-git add ${TMPDIR}; git commit -m 'update wiki'; git push
+cp -fv ${BASEDIR}/README.wiki ${TMPDIR}/Home.wiki >> ${BASEDIR}/.wiki.log 2>&1
+(cd ${TMPDIR}; git status -sb)
+(cd ${TMPDIR}; git add *; git commit -m 'updated'; git push) >> ${BASEDIR}/.wiki.log 2>&1
+(git add ${TMPDIR}; git commit -m 'update wiki'; git push) >> ${BASEDIR}/.wiki.log 2>&1
