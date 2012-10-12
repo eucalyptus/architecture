82bf96ea2d8427c6b223d1087f07e448c4b3a7e5
chris grzegorczyk <grze@eucalyptus.com>
2012-10-12 01:18:29 -0700

    draft

--- bin/publish.sh
+++ bin/publish.sh
@@ -16,15 +16,11 @@ egrep -v "${EXCLUDE_DIRS}"
 for f in  $(echo "${FILELIST}" | sed 's/^\.\///g'); do
 	t=:${f//\//:}
 	t=${t//features:/}
-	dest=${TMPDIR}/$(dirname ${t//://})/$t
-	dest_img=${TMPDIR}/$(dirname ${f//features/})/$(basename $f)
+	dest=${TMPDIR}/$t
   echo "$f => $dest ($dest_img)"
   mkdir -p $(dirname $dest)
-  cp -fv ${BASEDIR}/$f ${TMPDIR}/$dest >> ${BASEDIR}/.wiki.log >> ${BASEDIR}/.wiki.log 2>&1
-	if file ${BASEDIR}/$f | grep image >/dev/null 2>&1; then
-		cp -fv ${BASEDIR}/$f ${TMPDIR}/$dest >> ${BASEDIR}/.wiki.log >> ${BASEDIR}/.wiki.log 2>&1
-	fi
+  cp -fv ${BASEDIR}/$f $dest >> ${BASEDIR}/.wiki.log | tee ${BASEDIR}/.wiki.log 2>&1
 done
 cp -fv ${BASEDIR}/Home.wiki ${TMPDIR}/Home.wiki >> ${BASEDIR}/.wiki.log 2>&1
 (cd ${TMPDIR}; git add *; git status -sb; git commit -m 'updated'; git push) | tee ${BASEDIR}/.wiki.log 2>&1
-(git add ${TMPDIR}; git commit -m 'update wiki'; git push) >> ${BASEDIR}/.wiki.log 2>&1
+(git add ${TMPDIR}; git commit -m 'update wiki'; git push)  | tee ${BASEDIR}/.wiki.log 2>&1
