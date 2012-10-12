78b8795987e8a7593b0d6f03be81db07c2dc3d44
chris grzegorczyk <grze@eucalyptus.com>
2012-10-12 04:42:09 -0700

    draft

--- bin/publish.sh
+++ bin/publish.sh
@@ -7,7 +7,7 @@ EXCLUDE_DIRS="/bin/|/releases/|/lib/|/wiki/|/.git"
 
 echo > ${BASEDIR}/.wiki.log
 FILELIST=$(cd ${BASEDIR}; 
-find .  -type f  | 
+find features  -type f  | 
 egrep -v '^./wiki' | 
 egrep -v "${EXCLUDE}" | 
 egrep -v "\.(${EXCLUDE_SUFFIX})$" |
@@ -20,8 +20,16 @@ for f in  $(echo "${FILELIST}" | sed 's/^\.\///g'); do
   t=${dir//\//-}
   dest=${DESTDIR}/$t
   echo "$f => ${dir}/${file} ($t)"
-  cp -fv ${BASEDIR}/$f ${DESTDIR}/${dir//\//-}-${file} >> ${BASEDIR}/.wiki.log | tee ${BASEDIR}/.wiki.log 2>&1
+	if egrep '{{[^ ]*}}' $f >/dev/null 2>&1; then
+		(cd $dir; 
+			sed -nf ${BASEDIR}/bin/include.sed $file | sed 'N;N;s/\n//' | sed -f - $file > ${DESTDIR}/${dir//\//-}-${file}
+			)
+	else
+  	cp -fv ${BASEDIR}/$f ${DESTDIR}/${dir//\//-}-${file} >> ${BASEDIR}/.wiki.log | tee ${BASEDIR}/.wiki.log 2>&1
+	fi
 done
-cp -fv ${BASEDIR}/features/Home.wiki ${DESTDIR}/Home.wiki >> ${BASEDIR}/.wiki.log 2>&1
+(cd features; 
+	sed -nf ${BASEDIR}/bin/include.sed Home.wiki | sed 'N;N;s/\n//' | sed -f - Home.wiki > ${DESTDIR}/Home.wiki
+	)
 (cd ${DESTDIR}; git add ./*; git status -sb; git commit -m 'updated'; git push) | tee ${BASEDIR}/.wiki.log 2>&1
 (git add ${DESTDIR}; git commit -m 'update wiki'; git push)  | tee ${BASEDIR}/.wiki.log 2>&1
