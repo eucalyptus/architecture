d753e73aa0d73f1367897fa46085cb0fa8d6af3b
chris grzegorczyk <grze@eucalyptus.com>
2012-10-12 01:54:39 -0700

    update wiki

--- bin/publish.sh
+++ bin/publish.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 BASEDIR=$(git rev-parse --show-toplevel)
-TMPDIR=${BASEDIR}/wiki
+DESTDIR=${BASEDIR}/wiki
 EXCLUDE="TODO|README.wiki|.wiki.log|EUCA-....|Home.wiki|README.md"
 EXCLUDE_SUFFIX="pdf|zip|wsdl|git|puml|keep"
 EXCLUDE_DIRS="/bin/|/releases/|/lib/|/wiki/|/.git"
@@ -13,15 +13,15 @@ egrep -v "${EXCLUDE}" |
 egrep -v "\.(${EXCLUDE_SUFFIX})$" |
 egrep -v "${EXCLUDE_DIRS}"
 )
+(cd ${DESTDIR}; git checkout master)
 for f in  $(echo "${FILELIST}" | sed 's/^\.\///g'); do
   dir=$(dirname ${f//features\//})
   file=$(basename $f)
   t=${dir//\//-}
-  dest=${TMPDIR}/$t
+  dest=${DESTDIR}/$t
   echo "$f => ${dir}/${file} ($t)"
-  mkdir -p $(dirname ${TMPDIR}/${dir})
-  cp -fv ${BASEDIR}/$f ${dir}/${file} >> ${BASEDIR}/.wiki.log | tee ${BASEDIR}/.wiki.log 2>&1
+  cp -fv ${BASEDIR}/$f ${DESTDIR}/${dir//\//-}-${file} >> ${BASEDIR}/.wiki.log | tee ${BASEDIR}/.wiki.log 2>&1
 done
-cp -fv ${BASEDIR}/features/Home.wiki ${TMPDIR}/Home.wiki >> ${BASEDIR}/.wiki.log 2>&1
-(cd ${TMPDIR}; git add ./*; git status -sb; git commit -m 'updated'; git push) | tee ${BASEDIR}/.wiki.log 2>&1
-#(git add ${TMPDIR}; git commit -m 'update wiki'; git push)  | tee ${BASEDIR}/.wiki.log 2>&1
+cp -fv ${BASEDIR}/features/Home.wiki ${DESTDIR}/Home.wiki >> ${BASEDIR}/.wiki.log 2>&1
+(cd ${DESTDIR}; git add ./*; git status -sb; git commit -m 'updated'; git push) | tee ${BASEDIR}/.wiki.log 2>&1
+(git add ${DESTDIR}; git commit -m 'update wiki'; git push)  | tee ${BASEDIR}/.wiki.log 2>&1
