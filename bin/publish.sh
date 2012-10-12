304c21374396c52191b31e5a756de990eb1e7099
chris grzegorczyk <grze@eucalyptus.com>
2012-10-12 01:29:39 -0700

    draft

--- bin/publish.sh
+++ bin/publish.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 BASEDIR=$(git rev-parse --show-toplevel)
 TMPDIR=${BASEDIR}/wiki
-EXCLUDE="TODO|README.wiki|.wiki.log|EUCA-....|Home.wiki"
+EXCLUDE="TODO|README.wiki|.wiki.log|EUCA-....|Home.wiki|README.md"
 EXCLUDE_SUFFIX="pdf|zip|wsdl|git|puml|keep"
 EXCLUDE_DIRS="/bin/|/releases/|/lib/|/wiki/|/.git"
 
@@ -21,6 +21,6 @@ for f in  $(echo "${FILELIST}" | sed 's/^\.\///g'); do
   mkdir -p $(dirname $dest)
   cp -fv ${BASEDIR}/$f $dest >> ${BASEDIR}/.wiki.log | tee ${BASEDIR}/.wiki.log 2>&1
 done
-cp -fv ${BASEDIR}/Home.wiki ${TMPDIR}/Home.wiki >> ${BASEDIR}/.wiki.log 2>&1
-(cd ${TMPDIR}; git add *; git status -sb; git commit -m 'updated'; git push) | tee ${BASEDIR}/.wiki.log 2>&1
+cp -fv ${BASEDIR}/features/Home.wiki ${TMPDIR}/Home.wiki >> ${BASEDIR}/.wiki.log 2>&1
+(cd ${TMPDIR}; git add ./*; git status -sb; git commit -m 'updated'; git push) | tee ${BASEDIR}/.wiki.log 2>&1
 (git add ${TMPDIR}; git commit -m 'update wiki'; git push)  | tee ${BASEDIR}/.wiki.log 2>&1
