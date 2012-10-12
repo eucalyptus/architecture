7429b8ce4d077d1e78d16319af2338a266dcc5c1
chris grzegorczyk <grze@eucalyptus.com>
2012-10-12 01:12:38 -0700

    draft

--- bin/publish.sh
+++ bin/publish.sh
@@ -4,6 +4,7 @@ TMPDIR=${BASEDIR}/wiki
 EXCLUDE="TODO|README.wiki|.wiki.log|EUCA-....|Home.wiki"
 EXCLUDE_SUFFIX="pdf|zip|wsdl|git|puml|keep"
 EXCLUDE_DIRS="/bin/|/releases/|/lib/|/wiki/|/.git"
+
 echo > ${BASEDIR}/.wiki.log
 FILELIST=$(cd ${BASEDIR}; 
 find .  -type f  | 
@@ -16,9 +17,13 @@ for f in  $(echo "${FILELIST}" | sed 's/^\.\///g'); do
 	t=:${f//\//:}
 	t=${t//features:/}
 	dest=${TMPDIR}/$(dirname ${t//://})/$t
-  echo "$f => $dest"
+	dest_img=${TMPDIR}/$(dirname ${f//features/})/$(basename $f)
+  echo "$f => $dest ($dest_img)"
   mkdir -p $(dirname $dest)
   cp -fv ${BASEDIR}/$f ${TMPDIR}/$dest >> ${BASEDIR}/.wiki.log >> ${BASEDIR}/.wiki.log 2>&1
+	if file ${BASEDIR}/$f | grep image >/dev/null 2>&1; then
+		cp -fv ${BASEDIR}/$f ${TMPDIR}/$dest >> ${BASEDIR}/.wiki.log >> ${BASEDIR}/.wiki.log 2>&1
+	fi
 done
 cp -fv ${BASEDIR}/Home.wiki ${TMPDIR}/Home.wiki >> ${BASEDIR}/.wiki.log 2>&1
 (cd ${TMPDIR}; git add *; git status -sb; git commit -m 'updated'; git push) | tee ${BASEDIR}/.wiki.log 2>&1
